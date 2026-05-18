cfa9c663c92ac58537121ac723c4044898e34bb3
Vyshnavi Chhetri <vyshnavichhetri309@gmail.com>
2026-05-18 06:42:41 +0545

    Add Project Files

---
+++ controllers/listings.js
@@ -0,0 +1,110 @@
+const Listing = require("../models/listing");
+
+module.exports.index = async(req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {   
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+    .populate({
+        path: "reviews", 
+        populate: {
+            path: "author",
+        },
+     })
+     .populate("owner");
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exit!");
+        res.redirect("/listings");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res, next) =>{
+
+    const location =  req.body.listing.location;
+    console.log("Searching for:", location);
+
+    const geoUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json&limit=1`;
+    const response = await fetch(geoUrl, {
+        headers: {
+            "User-Agent" : "MERN-Map-App/1.0 (https://portfolio.com)"
+        }
+    }); 
+    
+    const data = await response.json();    
+    console.log("Geocoding result:", data);
+     if (!data.length) {
+        req.flash("error", "Location not found!");
+        return res.redirect("/listings/new");
+    }
+
+    const coords = {
+        lat: parseFloat(data[0].lat),
+        lon: parseFloat(data[0].lon)
+    };
+
+    console.log("Final Coords: ",coords);
+    // res.send("done!!");
+
+    let url = req.file.path;
+    let filename = req.file.filename;
+
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;   
+    newListing.image = {url, filename};  
+    
+    newListing.geometry = {
+        type: "Point",
+        coordinates: [coords.lon, coords.lat]
+    };
+
+    console.log(newListing.geometry);
+    
+    let savedListing =  await newListing.save();
+    console.log(savedListing);
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings"); 
+};
+
+module.exports.renderEditForm = async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exit!");
+        res.redirect("/listings");
+    }
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
+};
+
+module.exports.updateListing = async(req, res) =>{
+    
+    let { id } = req.params;
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if ( typeof req.file !== "undefined" ){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async(req,res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+};
---
+++ public/JS/map.js
@@ -0,0 +1,32 @@
+const mapDiv = document.getElementById("map");
+
+if (mapDiv) {
+    const coords = JSON.parse(mapDiv.dataset.coords);
+    const title = mapDiv.dataset.title;
+
+    console.log("MAP JS LOADED");
+
+    const map = new maplibregl.Map({
+        container: 'map',
+        style: "https://tiles.openfreemap.org/styles/bright",
+        center: coords,
+        zoom: 9,
+    });
+
+    const markerImg = document.createElement("img");
+    markerImg.src = "/images/airbnb.jpeg";
+    markerImg.classList.add("airbnb-marker");
+
+    new maplibregl.Marker({ element: markerImg })
+    .setLngLat(coords)
+    .setPopup(new maplibregl.Popup({ offset: 25 }).setHTML(
+        `<h4>${title}</h4><p>Exact Location will be provided after booking</p>`
+    ))
+    .addTo(map);
+
+    map.flyTo({
+        center: coords,
+        zoom: 14,
+    });
+   
+};
